23e0d4b71df0499c96eac8313fda8539d1d76e6c
Bart Vrancken <bart@abuse.io>
2015-06-12 13:46:46 +0200

    add legacy code with testresult

---
+++ config/shadowserver.php
@@ -0,0 +1,211 @@
+<?php
+
+return
+    [
+        'notifier' =>
+        [
+            'name'          => 'Shadowserver',
+            'enabled'       => true,
+            // Set sender and body mapping here, instead of main config and then merge into main?
+            'sender_map'    =>
+            [
+                '/autoreports@shadowserver.org/'
+            ],
+            'body_map'      =>
+            [
+                //
+            ],
+            'default'      =>
+            [
+                'class'     => 'Unknown classification',
+                'type'      => 'Abuse',
+                'enabled'   => false,
+            ],
+        ],
+        'feeds' =>
+        [
+            'scan_qotd' =>
+            [
+                'class'     => 'Open QOTD Server',
+                'fields'    => 'protocol port',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'spam_url' =>
+            [
+                'class'     => 'Spamvertised web site',
+                'fields'    => 'url host',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'scan_mssql' =>
+            [
+                'class'     => 'Open Microsoft SQL Server',
+                'fields'    => 'protocol port version instance_name tcp_port named_pipe response_length amplification',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_ssl_poodle' =>
+            [
+                'class'     => 'SSLv3 Vulnerable Server',
+                'fields'    => 'port handshake cipher_suite subject_common_name issuer_common_name cert_expiration_date issuer_organization_name',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'ssl_scan' =>
+            [
+                'class'     => 'SSLv3 Vulnerable Server',
+                'fields'    => 'port handshake cipher_suite subject_common_name issuer_common_name cert_expiration_date issuer_organization_name',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_ssl_freak' =>
+            [
+                'class'     => 'FREAK Vulnerable Server',
+                'fields'    => 'port handshake cipher_suite freak_cipher_suite subject_common_name issuer_common_name cert_expiration_date',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'cc_ip' =>
+            [
+                'class'     => 'Command and control server',
+                'fields'    => 'port channel',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'scan_ntpmonitor' =>
+            [
+                'class'     => 'Possible DDOS sending NTP Server',
+                'fields'    => 'protocol port hostname packets size',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'compromised_website' =>
+            [
+                'class'     => 'Compromised website',
+                'fields'    => 'http_host category tag redirect_target',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'cwsandbox_url' =>
+            [
+                'class'     => 'Malware infection',
+                'fields'    => 'md5hash url user_agent host method',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'sinkhole_http_drone' =>
+            [
+                'class'     => 'Botnet infection',
+                'fields'    => 'type url http_agent src_port dst_ip dst_port',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'microsoft_sinkhole' =>
+            [
+                'class'     => 'Botnet infection',
+                'fields'    => 'type url http_agent src_port dst_ip dst_port',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'botnet_drone' =>
+            [
+                'class'     => 'Botnet infection',
+                'fields'    => 'infection url agent cc cc_port cc_dns',
+                'type'      => 'ABUSE',
+                'enabled'   => true,
+            ],
+            'dns_openresolver' =>
+            [
+                'class'     => 'Open DNS Resolver',
+                'fields'    => 'protocol port min_amplification dns_version',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_ntp' =>
+            [
+                'class'     => 'Open NTP Server',
+                'fields'    => 'clock error frequency peer refid reftime stratum system',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_snmp' =>
+            [
+                'class'     => 'Open SNMP Server',
+                'fields'    => 'sysdesc sysname version',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_netbios' =>
+            [
+                'class'     => 'Open Netbios Server',
+                'fields'    => 'mac_address workgroup machine_name username',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_ssdp' =>
+            [
+                'class'     => 'Open SSDP Server',
+                'fields'    => 'systime location server unique_service_name',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_chargen' =>
+            [
+                'class'     => 'Open Chargen Server',
+                'fields'    => 'protocol port size',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_ipmi' =>
+            [
+                'class'     => 'Open IPMI Server',
+                'fields'    => 'port ipmi_version none_auth md2_auth md5_auth passkey_auth oem_auth defaultkg permessage_auth userlevel_auth usernames nulluser anon_login',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_net_pmp' =>
+            [
+                'class'     => 'Open NAT_PMP Server',
+                'fields'    => 'protocol port version uptime',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_nat_pmp' =>
+            [
+                'class'     => 'Open NAT_PMP Server',
+                'fields'    => 'protocol port version uptime',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_redis' =>
+            [
+                'class'     => 'Open REDIS Server',
+                'fields'    => 'protocol port version mode os process_id',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'wiki_file' =>
+            [
+                //Apparently shadowserver used this one in error, keeping it for parsing history
+                'class'     => 'Open MemCached Server',
+                'fields'    => 'protocol port version uptime curr_connections total_connections',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_memcached' =>
+            [
+                'class'     => 'Open MemCached Server',
+                'fields'    => 'protocol port version uptime curr_connections total_connections',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+            'scan_mongodb' =>
+            [
+                'class'     => 'Open MongoDB Server',
+                'fields'    => 'protocol port version sysinfo visible_databases',
+                'type'      => 'INFO',
+                'enabled'   => true,
+            ],
+        ],
+    ];
---
+++ src/shadowserver.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace AbuseIO\Parsers;
+
+use Illuminate\Config\Repository as ConfigRepository;
+
+class Shadowserver
+{
+
+    protected $config;
+
+    public $parsedMail;
+    public $arfMail;
+
+    public function __construct(/*ConfigRepository */ $config, $parsedMail, $arfMail)
+    {
+        $this->config = $config;
+        $this->parsedMail = $parsedMail;
+        $this->arfMail = $arfMail;
+    }
+
+    public function parse()
+    {
+
+        /*
+         * Returns with:
+         * [
+         * 'errorStatus' => $boolean required
+         * 'errorMessage' => $string required if errorStatus is true
+         * 'data' => $array required if errorStatus is false
+         * ]
+         */
+
+        /* Legacy code:
+        foreach ($message['attachments'] as $attachmentID => $attachment) {
+            preg_match("~(?:\d{4})-(?:\d{2})-(?:\d{2})-(.*)-[^\-]+-[^\-]+.csv~i", $attachment, $feed);
+            $feed = $feed[1];
+
+            if (!isset($feeds[$feed])) {
+                //Autodetect of classification failed - this is a config error!
+                logger(LOG_ERR, __FUNCTION__ . " A configuration error was detected. An unconfigured feed ${feed} was selected for parsing");
+                logger(LOG_WARNING, __FUNCTION__ . " FAILED message from ${source} subject ${message['subject']}");
+                return false;
+            } else if (in_array($feed, $feed_ignore)) {
+                logger(LOG_INFO, __FUNCTION__ . " IGNORING message from ${source} subject ${message['subject']}");
+                return true;
+            }
+
+            $class = $feeds[$feed]['class'];
+            $type = $feeds[$feed]['type'];
+            $fields = explode(" ", $feeds[$feed]['fields']);
+            $reports = csv_to_array("${message['store']}/${attachmentID}/${attachment}");
+
+            if (!is_array($reports)) {
+                logger(LOG_ERR, __FUNCTION__ . " A parser error was detected. Will not try to continue to parse this e-mail");
+                logger(LOG_WARNING, __FUNCTION__ . " FAILED message from ${source} subject ${message['subject']}");
+                return false;
+            }
+
+            foreach ($reports as $id => $report) {
+                $information = array();
+                foreach ($fields as $field) {
+                    if (!empty($report[$field])) {
+                        $information[$field] = $report[$field];
+                    }
+                }
+
+                $outReport = array(
+                    'source' => $source,
+                    'ip' => $report['ip'],
+                    'class' => $class,
+                    'type' => $type,
+                    'timestamp' => strtotime($report['timestamp']),
+                    'information' => $information
+                );
+
+                //These reports have a domain, which we want to register seperatly
+                if ($feed == "spam_url") {
+                    $url_info = parse_url($report['url']);
+
+                    $outReport['domain'] = $url_info['host'];
+                    $outReport['uri'] = $url_info['path'];
+                }
+                if ($feed == "ssl_scan") {
+                    $outReport['domain'] = $report['subject_common_name'];
+                    $outReport['uri'] = "/";
+                }
+                if ($feed == "compromised_website") {
+                    $outReport['domain'] = $report['http_host'];
+                    $outReport['uri'] = "/";
+                }
+                if ($feed == "botnet_drone") {
+                    $outReport['domain'] = $report['cc_dns'];
+                    $outReport['uri'] = str_replace("//", "/", "/" . $report['url']);
+                }
+
+                $reportID = reportAdd($outReport);
+                if (!$reportID) return false;
+                if (KEEP_EVIDENCE == true && $reportID !== true) {
+                    evidenceLink($message['evidenceid'], $reportID);
+                }
+
+            }
+        }
+        */
+
+        $events = [ ];
+
+        return
+            [
+                'errorStatus'   => false,
+                'errorMessage'  => 'test',
+                'data'          => $events,
+            ];
+
+    }
+}
